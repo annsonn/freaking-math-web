2be42e9ff5f90c30f610664faf259257b705b56b
Anson Chung <anson.chung@gmail.com>
2014-08-11 20:39:26 +0000

    bringing in angular timer

--- app/scripts/controllers/GameCtrl.js
+++ app/scripts/controllers/GameCtrl.js
@@ -15,25 +15,38 @@ angular.module('freakingMathWebApp')
     $scope.timer = '100';
     $scope.gameOver = false;
     
-    $scope.topScore = 0;
+    $scope.topScore = 0;      
     
     $scope.reset = function reset() {
       $scope.player = PlayerService.newPlayer();
       $scope.equation = MathService.makeEquation();
       $scope.gameOver = false;
+      $scope.startTimer();
     };
     
     $scope.validate = function validation(userAnswer) {
       if (userAnswer === $scope.equation.isAnswerCorrect) {
         $scope.player.score = $scope.player.score+1;
         $scope.equation = MathService.makeEquation();
+        $scope.$broadcast('timer-clear');
+        $scope.$broadcast('timer-start');
       }
       else {
         $scope.gameOver = true;
+        $scope.$broadcast('timer-stop');
         if ($scope.player.score > $scope.topScore) {
-          $scope.topScore = $scope.player.score;
+          $scope.topScore = $scope.player.score;          
         }
       }
     };
+    
+    $scope.startTimer = function (){
+      $scope.$broadcast('timer-start');
+    };
+    $scope.startTimer();
+    
+    $scope.$on('timer-stopped', function (){
+      $scope.gameOver = true;
+    });
     
   });
