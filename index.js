b9123de01fdba1c6605e99f1ed65b2f776b17c8e
Anson Chung <anson.chung@gmail.com>
2014-08-13 04:24:31 +0000

    adding logging

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var app = require('express')();
+var logfmt = require("logfmt");
 var http = require('http').Server(app);
 
+app.use(logfmt.requestLogger());
 app.use(require('express').static(process.cwd() + '/app'));
 
 app.get('/', function(req, res){
   res.sendfile('/app/index.html');
 });
 
-var port = process.env.PORT || 8080;
+var port = Number(process.env.PORT || 8080);
 
 http.listen(port, function(){
   console.log("Listening on " + port);
